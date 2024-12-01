7d53c044c38b2e7d987987348de59bdfafee631a
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-12-01 19:23:58 -0300

    Build different content depending on the internet connection

--- f2paradise/lib/pages/p_home.dart
+++ f2paradise/lib/pages/p_home.dart
@@ -11,8 +11,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Modify these variables depending on the result of internet connection
+  bool hasInternetConnection = false;
+  bool hasLocalData = false;
+
   @override
   Widget build(BuildContext context) {
+    if (hasInternetConnection) {
+      return buildOnlineContent();
+    } else if (!hasInternetConnection && hasLocalData) {
+      return buildOfflineWithLocalDataContent();
+    } else {
+      return buildNoInternetContent();
+    }
+  }
+
+  Widget buildOnlineContent() {
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -153,6 +167,226 @@ class _HomePageState extends State<HomePage> {
                     GameCard('gameName7', 'content7', 'assets/icons/i_bird.jpg'),
                     GameCard('gameName8', 'content8', 'assets/icons/i_bird.jpg'),
                     GameCard('gameName9', 'content9', 'assets/icons/i_bird.jpg'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildOfflineWithLocalDataContent() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.white)
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.favorite_border,
+              color: Theme.of(context).colorScheme.secondary
+            ),
+            onPressed: () {
+              // Add code here to navigate to Wishlist page.
+            },
+            tooltip: 'WishList',
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.info_outline,
+              color: Theme.of(context).colorScheme.secondary
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AboutPage()),
+              );
+            },
+            tooltip: 'About',
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Search bar.
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search_outlined,
+                          color: Theme.of(context).colorScheme.tertiary
+                        ),
+                        hintText: 'Busca por título, género, etc.',
+                        filled: true,
+                        fillColor: Color(0xFF232323),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide.none
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.filter_alt_outlined,
+                            color: Theme.of(context).colorScheme.tertiary,
+                          ),
+                          onPressed: () {
+                            // Add code here to display pop-up with tags to select.
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              // Tags for categories.
+              Text(
+                'Categorías',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    CategoryTag(label: 'Acción'),
+                    CategoryTag(label: 'Aventura'),
+                    CategoryTag(label: 'Estrategia'),
+                    CategoryTag(label: 'RPG'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              // Recents section.
+              Text(
+                'Recientes',
+                style: TextStyle(color: Colors.white)
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 200,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    GameCard('gameName1', 'content1', 'assets/icons/i_bird.jpg'),
+                    GameCard('gameName2', 'content2', 'assets/icons/i_bird.jpg'),
+                    GameCard('gameName3', 'content3', 'assets/icons/i_bird.jpg'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              // No internet image.
+              Center(
+                child: Image.asset(
+                  'assets/icons/i_bird.jpg',
+                  width: 150,
+                  height: 150,
+                ),
+              ),
+              SizedBox(height: 20),
+              // No internet message.
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'No hay conexión',
+                      style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Parece que no tienes conexión a Internet\nConéctate a una red para ver las demás secciones',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildNoInternetContent() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.favorite_border,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {
+              // Add code here to navigate to Wishlist page.
+            },
+            tooltip: 'WishList',
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.info_outline,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AboutPage()),
+              );
+            },
+            tooltip: 'About',
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 50),
+              // No internet image.
+              Center(
+                child: Image.asset(
+                  'assets/icons/i_bird.jpg',
+                  width: 150,
+                  height: 150,
+                ),
+              ),
+              SizedBox(height: 20),
+              // No internet message.
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'No hay conexión',
+                      style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Parece que no tienes conexión a Internet\nConéctate a una red e inténtalo de nuevo',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
                   ],
                 ),
               ),
